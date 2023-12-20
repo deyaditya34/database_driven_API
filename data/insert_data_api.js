8d99ca9f1219e3bdb5fa2ab00b044af34bf6bcb5
Aditya Dey <deyaditya34@gmail.com>
2023-12-20 21:12:35 +0530

    updated

--- data/insert_data_api.js
+++ data/insert_data_api.js
@@ -3,29 +3,28 @@ const { createData } = require("./data.service");
 const { parseCsvRecords } = require("../middlewares/csv_parser");
 
 async function controller(req, res) {
-  let uploadedFiles = req.file;
+  let uploadedFile = req.file;
   let itemCollector = [];
-
-  await parseCsvRecords(uploadedFiles.path, async (obj) => {
+  
+  await parseCsvRecords(uploadedFile.path, async (obj) => {
     let insertData;
     if (obj === null) {
       if (itemCollector.length) {
         insertData = await createData(itemCollector, req.body.collectionName);
-        console.log(data);
+        console.log(insertData.insertedCount, uploadedFile.path);
       }
-
       console.log("Finished Processing Records!!!");
     } else {
       itemCollector.push(obj);
-    
-      if (itemCollector.length === 10) {
+
+      if (itemCollector.length === 10000) {
         insertData = await createData(itemCollector, req.body.collectionName);
-        console.log(insertData.insertedCount);
+        console.log(insertData.insertedCount, uploadedFile.path);
         itemCollector = [];
       }
     }
   });
-
+  
   res.json({
     success: true,
   });
